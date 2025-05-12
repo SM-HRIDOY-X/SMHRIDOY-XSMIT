20cafc83870c43c2527704cd83fe9dd68ee24a48
SM-HRIDOY-X <smhridoyariyan76@gmail.com>
2025-05-12 20:57:59 +0600

    Update soromkore🙊.js

--- Script/commands/soromkore🙊.js
+++ Script/commands/soromkore🙊.js
@@ -1,35 +1,30 @@
+const fs = require("fs");
+
 module.exports.config = {
-	name: "🙊",
-	version: "1.0.1",
-	hasPermssion: 0,
-	credits: "Joshua Sy",
-	description: "scooby doo template memes",
-	commandCategory: "voice",
-	usages: "...",
-	cooldowns: 5,
-	dependencies: {
-	 "fs-extra": "",
-	 "axios": "",
-	 "canvas" :"",
-	 "jimp": "",
-	 "node-superfetch": ""
-	}
+  name: "🙊",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "Joshua Sy",
+  description: "Scooby Doo template memes",
+  commandCategory: "voice",
+  usages: "🙊",
+  cooldowns: 5,
+  dependencies: {}
 };
 
-handleEvent: function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-  const content = event.body ? event.body : '';
-  const body = content.toLowerCase();
-	if (body.indexOf("🙊")==0 || body.indexOf("🙊")==0 || body.indexOf("🫢")==0 || body.indexOf("🤭")==0) {
-		var msg = {
-				body: "আ্ঁমি্ঁ ব্ঁলু্ঁম্ঁ না্ঁ আ্ঁমা্ঁর্ঁ স্ঁর্ঁম্ঁ ক্ঁরে্ঁ,🤐🤭",
-				attachment: fs.createReadStream(__dirname + `/Hridoy/sorom .mp3`)
-			}
-			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😺", event.messageID, (err) => {}, true)
-		}
-	},
-	start: function({ nayan }) {
+module.exports.handleEvent = function ({ api, event }) {
+  const { threadID, messageID, body } = event;
+  const content = body ? body.toLowerCase() : "";
 
+  if (content.startsWith("🙊")  content.startsWith("🫢")  content.startsWith("🤭")) {
+    const msg = {
+      body: "আ্ঁমি্ঁ ব্ঁলু্ঁম্ঁ না্ঁ আ্ঁমা্ঁর্ঁ স্ঁর্ঁম্ঁ ক্ঁরে্ঁ,🤐🤭",
+      attachment: fs.createReadStream(__dirname + "/Hridoy/sorom.mp3")
+    };
+
+    api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😺", messageID, (err) => {}, true);
   }
-}
+};
+
+module.exports.run = function () {};
